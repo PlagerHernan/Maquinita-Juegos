ef8c6d261a37344fefe2ce6437b9f19deb5f27bf
PlagerHernan <plagerhernan@gmail.com>
2020-06-28 18:55:16 -0300

    Agregado LinkButton.cs

---
+++ Assets/Scripts/LinkButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkButton : MonoBehaviour
+{
+    public void OpenLink()
+    {
+        print("OpenLink()");
+        Application.OpenURL("http://www.hexar.org/");
+    }
+}
